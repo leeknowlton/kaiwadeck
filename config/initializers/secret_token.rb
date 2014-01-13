f0be2cde95a028277359bd5c9a116542e15b13e2
Lee Knowlton <leeknowlton@gmail.com>
2014-01-13 17:28:21 +0900

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kaiwadeck::Application.config.secret_key_base = '9324ecd4ec222f73787658204d7b384976612d354983868987bf7383b39b32acf6326a68972fb07b960c2a70c409c0423646186809a2b4fb6e803ad77d7a441e'
